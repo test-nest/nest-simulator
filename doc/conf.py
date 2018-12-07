274e10cf24a664036cda898f9f0732f3dd2a922e
Jessica Mitchell <j.mitchell@fz-juelich.de>
2018-12-07 11:36:41 +0100

    changing paths in conf

--- doc/conf.py
+++ doc/conf.py
@@ -51,9 +51,9 @@ from mock import Mock as MagicMock
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('./..'))
-sys.path.insert(0, os.path.abspath('./../topology'))
-sys.path.insert(0, os.path.abspath('./../pynest/nest'))
+sys.path.insert(0, os.path.abspath('.'))
+#sys.path.insert(0, os.path.abspath('./../topology'))
+#sys.path.insert(0, os.path.abspath('./../pynest/nest'))
 
 source_suffix = ['.rst', '.md']
 source_parsers = {
