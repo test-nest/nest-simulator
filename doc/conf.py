a18d7582379a4a255312ffc336dc3218d0fab44c
Steffen Graber <s.graber@fz-juelich.de>
2019-04-23 09:59:16 +0200

    Breathe works local an d on RTD

--- doc/conf.py
+++ doc/conf.py
@@ -136,11 +136,7 @@ breathe_default_project = "EXTRACT_MODELS"
 #    'backreferences_dir': False
 # }
 
-read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
-
-if read_the_docs_build:
-
-    subprocess.call('doxygen', shell=True)
+subprocess.call('doxygen', shell=True)
 
 mathjax_path = \
     "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.4/MathJax" \
