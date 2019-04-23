0d49141af7b32166b6abada2747e518199db2274
Steffen Graber <s.graber@fz-juelich.de>
2019-04-23 12:25:09 +0200

    delete xml

--- doc/conf.py
+++ doc/conf.py
@@ -51,9 +51,20 @@ from mock import Mock as MagicMock
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('./..'))
-sys.path.insert(0, os.path.abspath('./../topology'))
-sys.path.insert(0, os.path.abspath('./../pynest/nest'))
+
+
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+
+if on_rtd:
+	sys.path.insert(0, os.path.abspath('./..'))
+	sys.path.insert(0, os.path.abspath('./../topology'))
+	sys.path.insert(0, os.path.abspath('./../pynest/'))
+	sys.path.insert(0, os.path.abspath('./../pynest/nest'))
+	sys.path.insert(0, os.path.abspath('./../pynest/nest/lib'))
+else:
+	sys.path.insert(0, os.path.abspath('./..'))
+	sys.path.insert(0, os.path.abspath('./../topology'))
+	sys.path.insert(0, os.path.abspath('./../pynest/nest'))
 
 source_suffix = ['.rst', '.md']
 source_parsers = {
